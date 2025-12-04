7da75895bf22cfd68dc4e20c1e7b52581202a828
ken caruso <ken@ipl31.net>
2025-12-03 18:44:58 -0800

    fixup to handle to turns greater than 99

--- 1/main.py
+++ 1/main.py
@@ -11,13 +11,12 @@ def _turn_dial(dial: NDArray[Any], adjustment: str) -> NDArray[Any]:
     LEFT = "l"
     RIGHT = "r"
     direction, count = adjustment[:1].lower(), int(adjustment[1:])
-    print(f"Turn requested {adjustment} {direction} {count}")
+    print(f"Turn requested {adjustment}")
     assert direction in (LEFT, RIGHT)
-    assert count in range(0, 100)
     if direction == LEFT:
         count = -count
-        print(count)
     return np.roll(dial, count)
+
 
 def solve(turns: list) -> int:
     """Solve day 1, count times dial is in pos 0, start dial at 50"""
@@ -37,6 +36,8 @@ def test_e2e():
     turns = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
     assert solve(turns) == 3
 
+
 if __name__ == "__main__":
-    turns = []
-    print(f"Password is {solve(turns)}")
+    with open("input.txt", "r") as fp:
+        count = solve(fp.readlines())
+        print(f"Password is {count}")
